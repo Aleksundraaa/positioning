2e082dd5ade2366dde176adec695856fcc15c482
Aleksundraaa <ladycaxap@gmail.com>
2026-04-07 19:41:52 +0500

    номера 3-4

--- index.js
+++ index.js
@@ -5,3 +5,20 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+const progressFill = document.querySelector('.progress-fill');
+
+let currentProgress = 0;
+const duration = 3000;
+const updateTime = 20;
+const steps = duration / updateTime;
+const incProcent = 100 / steps;
+
+const timer = setInterval(() => {
+    currentProgress += incProcent;
+    progressFill.style.width = `${currentProgress}%`;
+
+    if (currentProgress >= 100) {
+        progressFill.style.width = `100%`;
+        clearInterval(timer);
+    }
+}, updateTime);
