c1ade8aa972d9a5fb7f3159c115bae6c11bcc8ac
anuri1 <antipenkovartem1997@gmail.com>
2026-04-07 19:25:15 +0500

    task 1

--- index.js
+++ index.js
@@ -5,3 +5,23 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+const modal = document.getElementById('window');
+const overlay = document.getElementById('overlay');
+const btn = document.getElementById('open-modal');
+const close = document.querySelector('.close');
+
+btn.onclick = function() {
+    modal.style.display = 'block';
+    overlay.style.display = 'block';
+};
+
+close.onclick = function() {
+    modal.style.display = 'none';
+    overlay.style.display = 'none';
+};
+
+overlay.onclick = function() {
+    modal.style.display = 'none';
+    overlay.style.display = 'none';
+};
